1ec1210063ce306af926d667a2f8581fb140e8e4
Alexey Gavrilov <a.gavrilov70@gmail.com>
2022-08-04 22:21:53 +0300

    Добавлены  задачи 3, 4, 5

---
+++ task004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = N* (- 1);
+while (M <= N)
+{
+    Console.Write(M+" ");
+    M++;
+}
+Console.WriteLine();
---
+++ task005/Program.cs
@@ -0,0 +1,7 @@
+﻿int N = 0;
+while(N < 100 || N >999)
+{
+Console.WriteLine("Введите трехзначное число: ");
+N = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Последняя цифра числа: " + N % 10);
