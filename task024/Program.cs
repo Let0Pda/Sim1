4644dbb68e7071c660f563c158ea28564f47ccd9
Let0Pda <a.gavrilov70@gmail.com>
2022-08-15 21:43:06 +0300

    Добавил решения

---
+++ task024/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Console.Write("Ввод числа А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i < number; i++)
+{
+    sum = sum + i;
+}
+Console.Write($"Результат работы программы = {sum}");
+*/
+
+int Cycle(int a)
+{
+    for (int i = 0; i < length; i++)
+    {
+
+    }
+}
