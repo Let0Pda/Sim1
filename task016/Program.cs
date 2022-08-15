eb57d10b975e0fcaa6ec89de4abb2ab9cad40de0
Let0Pda <a.gavrilov70@gmail.com>
2022-08-15 18:51:48 +0300

    задача 16 , переделал на случайное число и добавил функцию

--- task016/Program.cs
+++ task016/Program.cs
@@ -3,6 +3,7 @@
 -4, 16 -> да
 25, 5 -> да
 8,9 -> нет */
+/*
 Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
@@ -15,3 +16,22 @@ else
 {
     Console.Write("Нет");
 }
+*/
+
+Console.Clear();
+Console.WriteLine();
+
+int[] num = new int[2];
+string Result = "не является";
+
+for (int i = 0; i < 2; i++)
+{
+    num[i] = new Random().Next(1, 1000);
+    Console.WriteLine($"{num[i]}");
+}
+
+
+if (num[0] / num[1] == num[1]) Result = Result.Replace("не ", "");
+
+Console.WriteLine($"Число {num[0]} {Result} квадратом числа {num[1]}");
+Console.WriteLine();
