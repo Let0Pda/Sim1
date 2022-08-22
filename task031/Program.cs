0818fa80474f414b07f3a98825c9f3e8bd8a2ef3
Let0Pda <a.gavrilov70@gmail.com>
2022-08-22 19:24:30 +0300

    Задачи 31 и 37 разобрал

--- task031/Program.cs
+++ task031/Program.cs
@@ -25,17 +25,19 @@
 // }
 // Console.Write($"Сумма положительных чисел = {sumPositive}, а отрицательных {sumNegative}");
 
-int fillAray(int size, int leftRange, int rinhtRange)
+//С ипользованием функции
+
+int[] FillArray(int size, int leftRange, int rightRange)
 {
     Random rand = new Random();
     int[] arr = new int[size];
-
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = rand.Next(leftRange, rinhtRange + 1);
+        arr[i] = rand.Next(leftRange, rightRange + 1);
     }
     return arr;
 }
+
 void sumPositiveAndsumNegative(int[] arr, out int sumPositive, out int sumNegative)
 {
     sumPositive = 0;
@@ -52,54 +54,8 @@ void sumPositiveAndsumNegative(int[] arr, out int sumPositive, out int sumNegati
         }
     }
 }
-int array = fillAray(5, -9, 9);
+
+int[] array = FillArray(5, -9, 9);
 sumPositiveAndsumNegative(array, out int sumP, out int sumN);
 Console.WriteLine('[' + string.Join(", ", array) + ']');
-Console.WriteLine($"Сумма положительных чисел = {sumP}, а отрицательных ={sumN}");
-
-
-
-/*
-int n = 12;
-int min = -9;
-int max = 9;
-
-int[] CreateArray(int n, int min, int max)
-{
-    int[] array = new int[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        array[i] = new Random().Next(min, max + 1);
-    }
-    return array;
-}
-
-int[] PutSums(int[] arr)
-{
-    int[] sums = new int[2];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] < 0) sums[0] += arr[i];
-        else sums[1] += arr[i];
-    }
-    return sums;
-}
-
-void PritArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write($"{array[i]} ");
-    }
-    System.Console.WriteLine();
-}
-
-PritArray(PutSums(CreateArray(n, min, max)));
-
-// если нужно вывести сам массив, то так:
-
-int[] array = CreateArray(n, min, max);
-PritArray(array);
-PritArray(PutSums(array));
-*/
+Console.WriteLine($"Сумма положительных чисел = {sumP}, отрицательных = {sumN}");
