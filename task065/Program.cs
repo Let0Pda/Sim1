583bfdc9fe2ad485b63bfac38c9cbc538d6220bc
Let0Pda <a.gavrilov70@gmail.com>
2022-09-04 21:03:02 +0300

    Семинар 9

---
+++ task065/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выведет все натуральные числа от M до N
+
+int N = 5;
+int M = 100;
+void PrintNumbers(int number1, int number2)
+{
+    if (number1 > number2)
+    {
+        return;
+    }
+    Console.Write($"{number1} ");
+    PrintNumbers(number1 + 1, number2);
+}
+PrintNumbers(N, M);
